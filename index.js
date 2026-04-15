dea6aad67069542d4e21a7d739a300d2ecf96881
Aswini <134166292+aswinicse2002@users.noreply.github.com>
2026-04-15 22:51:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,40 @@
 /**
  *
- * OrderMeta
+ * OrderSummary
  *
  */
 
 import React from 'react';
 
-import { Row, Col } from 'reactstrap';
+import { Col } from 'reactstrap';
 
-import { CART_ITEM_STATUS } from '../../../constants';
-import { formatDate } from '../../../utils/date';
-import Button from '../../Common/Button';
-import { ArrowBackIcon } from '../../Common/Icon';
-
-const OrderMeta = props => {
-  const { order, cancelOrder, onBack } = props;
-
-  const renderMetaAction = () => {
-    const isNotDelivered =
-      order.products.filter(i => i.status === CART_ITEM_STATUS.Delivered)
-        .length < 1;
-
-    if (isNotDelivered) {
-      return <Button size='sm' text='Cancel Order' onClick={cancelOrder} />;
-    }
-  };
+const OrderSummary = props => {
+  const { order } = props;
 
   return (
-    <div className='order-meta'>
-      <div className='d-flex align-items-center justify-content-between mb-3 title'>
-        <h2 className='mb-0'>Order Details</h2>
-        <Button
-          variant='link'
-          icon={<ArrowBackIcon />}
-          size='sm'
-          text='Back to orders'
-          onClick={onBack}
-        ></Button>
+    <Col className='order-summary pt-3'>
+      <h2>Order Summary</h2>
+      <div className='d-flex align-items-center summary-item'>
+        <p className='summary-label'>Subtotal</p>
+        <p className='summary-value ml-auto'>${order.total}</p>
+      </div>
+      <div className='d-flex align-items-center summary-item'>
+        <p className='summary-label'>Est. Sales Tax</p>
+        <p className='summary-value ml-auto'>${order.totalTax}</p>
       </div>
 
-      <Row>
-        <Col xs='12' md='8'>
-          <Row>
-            <Col xs='4'>
-              <p className='one-line-ellipsis'>Order ID</p>
-            </Col>
-            <Col xs='8'>
-              <span className='order-label one-line-ellipsis'>{` ${order._id}`}</span>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs='4'>
-              <p className='one-line-ellipsis'>Order Date</p>
-            </Col>
-            <Col xs='8'>
-              <span className='order-label one-line-ellipsis'>{` ${formatDate(
-                order.created
-              )}`}</span>
-            </Col>
-          </Row>
-        </Col>
-        <Col xs='12' md='4' className='text-left text-md-right'>
-          {renderMetaAction()}
-        </Col>
-      </Row>
-    </div>
+      <div className='d-flex align-items-center summary-item'>
+        <p className='summary-label'>Shipping & Handling</p>
+        <p className='summary-value ml-auto'>$0</p>
+      </div>
+
+      <hr />
+      <div className='d-flex align-items-center summary-item'>
+        <p className='summary-label'>Total</p>
+        <p className='summary-value ml-auto'>${order.totalWithTax}</p>
+      </div>
+    </Col>
   );
 };
 
-export default OrderMeta;
+export default OrderSummary;
