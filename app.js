26a4e6f43460a4417cdca5af12dae7632e205989
Aswini <134166292+aswinicse2002@users.noreply.github.com>
2026-04-15 22:31:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,56 +1,9 @@
-/**
- *
- * app.js
- *
- */
+import { ROLES, EMAIL_PROVIDER } from '../constants';
 
-import React from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+export const isProviderAllowed = provider =>
+  provider === EMAIL_PROVIDER.Google || provider === EMAIL_PROVIDER.Facebook;
 
-import store, { history } from './store';
-import { SocketProvider } from './contexts/Socket';
-import { SET_AUTH } from './containers/Authentication/constants';
-import Application from './containers/Application';
-import ScrollToTop from './scrollToTop';
-import setToken from './utils/token';
-
-// Import application sass styles
-import './styles/style.scss';
-
-// Import Font Awesome Icons Set
-import 'font-awesome/css/font-awesome.min.css';
-
-// Import Simple Line Icons Set
-import 'simple-line-icons/css/simple-line-icons.css';
-
-// react-bootstrap-table2 styles
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-
-// rc-slider style
-import 'rc-slider/assets/index.css';
-
-// Authentication
-const token = localStorage.getItem('token');
-
-if (token) {
-  // authenticate api authorization
-  setToken(token);
-
-  // authenticate routes
-  store.dispatch({ type: SET_AUTH });
-}
-
-const app = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <SocketProvider>
-        <ScrollToTop>
-          <Application />
-        </ScrollToTop>
-      </SocketProvider>
-    </ConnectedRouter>
-  </Provider>
-);
-
-export default app;
+export const isDisabledMerchantAccount = user =>
+  user.role === ROLES.Merchant &&
+  user.merchant &&
+  user.merchant.isActive === false;
