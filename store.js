912373e0555eaef3778c086099249da082cc2996
Aswini <134166292+aswinicse2002@users.noreply.github.com>
2026-04-15 22:33:54 +0530

    Add files via upload

--- store.js
+++ store.js
@@ -1,44 +1,5 @@
-/**
- *
- * store.js
- * store configuration
- */
-
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { routerMiddleware } from 'connected-react-router';
-import { createBrowserHistory } from 'history';
-
-import createReducer from './reducers';
-
-export const history = createBrowserHistory({
-  basename: '/',
-  hashType: 'noslash'
-});
-
-const middlewares = [thunk, routerMiddleware(history)];
-
-const enhancers = [applyMiddleware(...middlewares)];
-
-// If Redux DevTools Extension is installed use it, otherwise use Redux compose
-const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const store = createStore(
-  createReducer(history),
-  composeEnhancers(...enhancers)
-);
-
-if (module.hot) {
-  // Enable Webpack hot module replacement for reducers
-  module.hot.accept('./reducers', () => {
-    const nextRootReducer = require('./reducers').default; // eslint-disable-line global-require
-    store.replaceReducer(nextRootReducer(history));
-  });
-}
-
-export default store;
+export const sortOptions = [
+  { value: 0, label: 'Newest First' },
+  { value: 1, label: 'Price High to Low' },
+  { value: 2, label: 'Price Low to High' }
+];
