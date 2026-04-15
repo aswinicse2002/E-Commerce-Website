3e55270520f93bf3baf8a76ad77adb7620a4aa22
Aswini <134166292+aswinicse2002@users.noreply.github.com>
2026-04-15 22:57:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,73 +1,101 @@
 /**
  *
- * WishList
+ * AccountDetails
  *
  */
 
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Row, Col } from 'reactstrap';
 
-import { formatDate } from '../../../utils/date';
+import { EMAIL_PROVIDER } from '../../../constants';
+import UserRole from '../UserRole';
+import Input from '../../Common/Input';
 import Button from '../../Common/Button';
-import { XIcon } from '../../Common/Icon';
 
-const WishList = props => {
-  const { wishlist, updateWishlist } = props;
+const AccountDetails = props => {
+  const { user, accountChange, updateProfile } = props;
 
-  const getProductImage = item => {
-    if (item.product) {
-      const product = item.product;
-      return (
-        <div className='d-flex flex-column justify-content-center align-items-center'>
-          <img
-            className='item-image'
-            src={`${
-              product.imageUrl
-                ? product.imageUrl
-                : '/images/placeholder-image.png'
-            }`}
-          />
-        </div>
-      );
-    }
+  const handleSubmit = event => {
+    event.preventDefault();
+    updateProfile();
   };
 
   return (
-    <div className='w-list'>
-      {wishlist.map((item, index) => (
-        <div
-          key={index}
-          className='d-flex flex-row align-items-center mx-0 mb-3 wishlist-box'
-        >
-          <Link
-            to={`/product/${item.product.slug}`}
-            key={index}
-            className='d-flex flex-1 align-items-center text-truncate'
-          >
-            {getProductImage(item)}
-            <div className='d-flex flex-column justify-content-center px-3 text-truncate'>
-              <h4 className='text-truncate'>{item.product.name}</h4>
-              <p className='mb-2 price'>${item.product.price}</p>
-              <label className='text-truncate'>{`Wishlist Added on ${formatDate(
-                item.created
-              )}`}</label>
-            </div>
-          </Link>
-          <div className='remove-wishlist-box'>
-            <Button
-              variant='danger'
-              icon={<XIcon className='text-white' width={15} />}
-              round={20}
-              onClick={e => {
-                updateWishlist(!item.isLiked, item.product._id);
+    <div className='account-details'>
+      <div className='info'>
+        <div className='desc'>
+          <p className='one-line-ellipsis mr-3'>
+            {user.provider === EMAIL_PROVIDER.Email ? (
+              user.email
+            ) : (
+              <span className='provider-email'>
+                Logged in With {user.provider}
+              </span>
+            )}
+          </p>
+          <UserRole user={user} />
+        </div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <Row>
+          <Col xs='12' md='6'>
+            <Input
+              type={'text'}
+              label={'First Name'}
+              name={'firstName'}
+              placeholder={'Please Enter Your First Name'}
+              value={user.firstName ? user.firstName : ''}
+              onInputChange={(name, value) => {
+                accountChange(name, value);
               }}
             />
-          </div>
+          </Col>
+          <Col xs='12' md='6'>
+            <Input
+              type={'text'}
+              label={'Last Name'}
+              name={'lastName'}
+              placeholder={'Please Enter Your Last Name'}
+              value={user.lastName ? user.lastName : ''}
+              onInputChange={(name, value) => {
+                accountChange(name, value);
+              }}
+            />
+          </Col>
+          {/* TODO: update email feature to be added instead form change */}
+          {/* <Col xs='12' md='6'>
+            <Input
+              type={'text'}
+              label={'Email'}
+              name={'email'}
+              placeholder={'Please Enter Your Email'}
+              value={user.email ? user.email : ''}
+              onInputChange={(name, value) => {
+                accountChange(name, value);
+              }}
+            />
+          </Col> */}
+          <Col xs='12' md='12'>
+            <Input
+              type={'text'}
+              label={'Phone Number'}
+              name={'phoneNumber'}
+              placeholder={'Please Enter Your Phone Number'}
+              value={user.phoneNumber ? user.phoneNumber : ''}
+              onInputChange={(name, value) => {
+                accountChange(name, value);
+              }}
+            />
+          </Col>
+        </Row>
+        <hr />
+        <div className='profile-actions'>
+          <Button type='submit' variant='secondary' text='Save changes' />
         </div>
-      ))}
+      </form>
     </div>
   );
 };
 
-export default WishList;
+export default AccountDetails;
