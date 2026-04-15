2f2bc98d8e93642896c79d1958bcac92ab2d1ed7
Aswini <134166292+aswinicse2002@users.noreply.github.com>
2026-04-15 22:57:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,62 @@
 /**
  *
- * AccountDetails
+ * AccountMenu
  *
  */
 
 import React from 'react';
 
-import { Row, Col } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Collapse, Navbar } from 'reactstrap';
 
-import { EMAIL_PROVIDER } from '../../../constants';
-import UserRole from '../UserRole';
-import Input from '../../Common/Input';
 import Button from '../../Common/Button';
 
-const AccountDetails = props => {
-  const { user, accountChange, updateProfile } = props;
+const AccountMenu = props => {
+  const { user, isMenuOpen, links, toggleMenu } = props;
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    updateProfile();
+  const getAllowedProvider = link => {
+    if (!link.provider) return true;
+
+    const userProvider = user.provider ?? '';
+    if (!userProvider) return true;
+
+    return link.provider.includes(userProvider);
   };
 
   return (
-    <div className='account-details'>
-      <div className='info'>
-        <div className='desc'>
-          <p className='one-line-ellipsis mr-3'>
-            {user.provider === EMAIL_PROVIDER.Email ? (
-              user.email
-            ) : (
-              <span className='provider-email'>
-                Logged in With {user.provider}
-              </span>
-            )}
-          </p>
-          <UserRole user={user} />
-        </div>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <Row>
-          <Col xs='12' md='6'>
-            <Input
-              type={'text'}
-              label={'First Name'}
-              name={'firstName'}
-              placeholder={'Please Enter Your First Name'}
-              value={user.firstName ? user.firstName : ''}
-              onInputChange={(name, value) => {
-                accountChange(name, value);
-              }}
-            />
-          </Col>
-          <Col xs='12' md='6'>
-            <Input
-              type={'text'}
-              label={'Last Name'}
-              name={'lastName'}
-              placeholder={'Please Enter Your Last Name'}
-              value={user.lastName ? user.lastName : ''}
-              onInputChange={(name, value) => {
-                accountChange(name, value);
-              }}
-            />
-          </Col>
-          {/* TODO: update email feature to be added instead form change */}
-          {/* <Col xs='12' md='6'>
-            <Input
-              type={'text'}
-              label={'Email'}
-              name={'email'}
-              placeholder={'Please Enter Your Email'}
-              value={user.email ? user.email : ''}
-              onInputChange={(name, value) => {
-                accountChange(name, value);
-              }}
-            />
-          </Col> */}
-          <Col xs='12' md='12'>
-            <Input
-              type={'text'}
-              label={'Phone Number'}
-              name={'phoneNumber'}
-              placeholder={'Please Enter Your Phone Number'}
-              value={user.phoneNumber ? user.phoneNumber : ''}
-              onInputChange={(name, value) => {
-                accountChange(name, value);
-              }}
-            />
-          </Col>
-        </Row>
-        <hr />
-        <div className='profile-actions'>
-          <Button type='submit' variant='secondary' text='Save changes' />
-        </div>
-      </form>
+    <div className='panel-sidebar'>
+      <Button
+        text='Dashboard Menu'
+        className={`${isMenuOpen ? 'menu-panel' : 'menu-panel collapse'}`}
+        ariaExpanded={isMenuOpen ? 'true' : 'false'}
+        // ariaLabel={isMenuOpen ? 'dashboard menu expanded' : 'dashboard menu collapse'}
+        onClick={toggleMenu}
+      />
+      <h3 className='panel-title'>Account</h3>
+      <Navbar color='light' light expand='md'>
+        <Collapse isOpen={isMenuOpen} navbar>
+          <ul className='panel-links'>
+            {links.map((link, index) => {
+              const PREFIX = link.prefix ? link.prefix : '';
+              const isProviderAllowed = getAllowedProvider(link);
+              if (!isProviderAllowed) return;
+              return (
+                <li key={index}>
+                  <NavLink
+                    to={PREFIX + link.to}
+                    activeClassName='active-link'
+                    exact
+                  >
+                    {link.name}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </Collapse>
+      </Navbar>
     </div>
   );
 };
 
-export default AccountDetails;
+export default AccountMenu;
